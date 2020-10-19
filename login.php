d8f7c8585e2d8e7b27f857adb0b771efb04c1b05
ali neji <72573756+alinj166@users.noreply.github.com>
2020-10-19 22:57:35 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,16 +1,17 @@
 <?php 
 
-mysql_connect("localhost","id15152509_alineji","Chayouba123/") ;
-mysql_select_db('id15152509_login');
+mysql_connect("localhost","$$$$$$$$$","$$$$$$$$$") ;
+mysql_select_db('$$$$$$$$$');
 
 $email=$_POST['t1']; 
-$pass=$_POST['t2'];
+$pas=$_POST['t2'];
 
-$req='select * from loginn where "$email"=email and "$pass"=pass;';
+$req="select * from loginn where '$email'=email and '$pas'=pass;";
 $res=mysql_query($req);
 if (mysql_num_rows($res)>0)
-echo(' login fait en succees');
+echo('<center><H1>Login Was Successful</H1></center>');
 else 
-echo('we don"t have account with that email:'.$email);
+	
+echo('<center><H1>The account was not found:'.$email.'</center></H1>');
  	
 ?>
